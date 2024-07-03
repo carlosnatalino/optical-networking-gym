5e514df6452aaa2e994b577c4a3c54284f6906ce
Carlos Natalino <6130746+carlosnatalino@users.noreply.github.com>
2024-07-03 09:49:43 +0200

    Fixing missing `install_requires` in the setup script.

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
 from setuptools import setup
-import numpy as np
+# import numpy as np
 from Cython.Build import cythonize
-
-debug_cythonize_kw = dict(force=True)
-
-debug_cythonize_kw.update(dict(gdb_debug=True,
-                              force=True,
-                              annotate=True,
-                              compiler_directives={'linetrace': True, 'binding': True}))
 
 setup(
     name="optical_networking_gym",
+    install_requires=["gymnasium", "numpy", "matplotlib", "networkx"],
     ext_modules=cythonize(
         [
             # "optical_networking_gym/envs/*.pyx",
             "optical_networking_gym/**/*.pyx",
         ],
-        include_path=[np.get_include()],
+        # include_path=[np.get_include()],
         language_level="3",
-        **debug_cythonize_kw
     ),
 )
