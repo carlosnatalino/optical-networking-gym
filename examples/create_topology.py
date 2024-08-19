83b65347d342451b97af43934eb6bfdc68012198
Carlos Natalino <6130746+carlosnatalino@users.noreply.github.com>
2024-08-19 10:54:16 +0200

    Implementing the initial generation of topologies

---
+++ examples/create_topology.py
@@ -0,0 +1,260 @@
+import argparse
+import pathlib
+import pickle
+from typing import Optional, Tuple
+
+import networkx as nx
+
+from graph_utils import read_sndlib_topology, read_txt_file
+
+from optical_networking_gym.topology import (
+    Span,
+    Link,
+    Modulation,
+    Path,
+    get_k_shortest_paths,
+    get_path_weight,
+    get_best_modulation_format_by_length,
+)
+
+# in case you do not have modulations
+cur_modulations: Optional[Tuple[Modulation]] = None
+
+# note that minimum OSNR and in-band cross-talk are optional parameters
+
+# example of in-band cross-talk settings for different modulation formats:
+# https://ieeexplore.ieee.org/abstract/document/7541954
+# table III
+
+# defining the EON parameters
+# definitions according to:
+# https://github.com/xiaoliangchenUCD/DeepRMSA/blob/eb2f2442acc25574e9efb4104ea245e9e05d9821/K-SP-FF%20benchmark_NSFNET.py#L268
+# cur_modulations = (
+#     # the first (lowest efficiency) modulation format needs to have maximum length
+#     # greater or equal to the longest path in the topology.
+#     # Here we put 100,000 km to be on the safe side
+#     Modulation(
+#         name="BPSK", maximum_length=100_000, spectral_efficiency=1, minimum_osnr=12.6, inband_xt=-14
+#     ),
+#     Modulation(
+#         name="QPSK", maximum_length=2_000, spectral_efficiency=2, minimum_osnr=12.6, inband_xt=-17
+#     ),
+#     Modulation(
+#         name="8QAM", maximum_length=1_250, spectral_efficiency=3, minimum_osnr=18.6, inband_xt=-20
+#     ),
+#     Modulation(
+#         name="16QAM", maximum_length=625, spectral_efficiency=4, minimum_osnr=22.4, inband_xt=-23
+#     ),
+# )
+
+# other setup:
+cur_modulations = (
+    # the first (lowest efficiency) modulation format needs to have maximum length
+    # greater or equal to the longest path in the topology.
+    # Here we put 100,000 km to be on the safe side
+    Modulation(
+        name="BPSK",
+        maximum_length=100_000,
+        spectral_efficiency=1,
+        minimum_osnr=12.6,
+        inband_xt=-14,
+    ),
+    Modulation(
+        name="QPSK",
+        maximum_length=2_000,
+        spectral_efficiency=2,
+        minimum_osnr=12.6,
+        inband_xt=-17,
+    ),
+    Modulation(
+        name="8QAM",
+        maximum_length=1_000,
+        spectral_efficiency=3,
+        minimum_osnr=18.6,
+        inband_xt=-20,
+    ),
+    Modulation(
+        name="16QAM",
+        maximum_length=500,
+        spectral_efficiency=4,
+        minimum_osnr=22.4,
+        inband_xt=-23,
+    ),
+    Modulation(
+        name="32QAM",
+        maximum_length=250,
+        spectral_efficiency=5,
+        minimum_osnr=26.4,
+        inband_xt=-26,
+    ),
+    Modulation(
+        name="64QAM",
+        maximum_length=125,
+        spectral_efficiency=6,
+        minimum_osnr=30.4,
+        inband_xt=-29,
+    ),
+)
+
+
+def get_topology(
+    file_path: str,
+    topology_name: str,
+    modulations: Optional[Tuple[Modulation]] = None,
+    max_span_length: float = 100,
+    default_attenuation: float = 0.2,
+    default_noise_figure: float = 4.5,
+    k_paths: int = 5
+) -> nx.Graph:
+    """
+    Function
+    """
+    k_shortest_paths = {}
+    if file_path.endswith(".xml"):
+        topology = read_sndlib_topology(file_path)
+    elif file_path.endswith(".txt"):
+        topology = read_txt_file(file_path)
+    else:
+        raise ValueError("Supplied topology format is unknown")
+
+    # generating the spans
+    topology.graph["has_links_object"] = True
+    for node1, node2 in topology.edges():
+        length = topology[node1][node2]["length"]
+        num_spans = int(length // max_span_length)
+        if length % num_spans != 0:
+            num_spans += 1
+        # print(f"{num_spans=}")
+        span_length = length / num_spans
+        spans = []
+        for _ in range(num_spans):
+            span = Span(
+                length=span_length,
+                attenuation=default_attenuation,
+                noise_figure=default_noise_figure,
+            )
+            spans.append(span)
+
+        link = Link(
+            id=topology[node1][node2]["index"],
+            length=length,
+            node1=node1,
+            node2=node2,
+            spans=tuple(spans),
+        )
+        topology[node1][node2]["link"] = link
+        # print(link)
+
+    idp = 0
+    for idn1, n1 in enumerate(topology.nodes()):
+        for idn2, n2 in enumerate(topology.nodes()):
+            if idn1 < idn2:
+                paths = get_k_shortest_paths(topology, n1, n2, k_paths, weight="length")
+                print(n1, n2, len(paths))
+                lengths = [
+                    get_path_weight(topology, path, weight="length") for path in paths
+                ]
+                if modulations is not None:
+                    selected_modulations = [
+                        get_best_modulation_format_by_length(length, modulations)
+                        for length in lengths
+                    ]
+                else:
+                    selected_modulations = [None for _ in lengths]
+                objs = []
+
+                for path, length, modulation in zip(
+                    paths, lengths, selected_modulations
+                ):
+                    objs.append(
+                        Path(
+                            idp,
+                            tuple(path),
+                            length,
+                            modulation,
+                        )
+                    )  # <== The topology is created and a best modulation is just automatically attached.  In our new implementation, the best modulation will be variable depending on available resources and the amount of crosstalk it will cause.
+                    print("\t", objs[-1])
+                    idp += 1
+                k_shortest_paths[n1, n2] = objs
+                k_shortest_paths[n2, n1] = objs
+    topology.graph["name"] = topology_name
+    topology.graph["ksp"] = k_shortest_paths
+    if modulations is not None:
+        topology.graph["modulations"] = modulations
+    topology.graph["k_paths"] = k_paths
+    topology.graph["node_indices"] = []
+    for idx, node in enumerate(topology.nodes()):
+        topology.graph["node_indices"].append(node)
+        topology.nodes[node]["index"] = idx
+    return topology
+
+
+if __name__ == "__main__":
+    # default values
+    default_k_paths = 5
+    default_topology_file = "nsfnet_chen.txt"
+    default_max_span_length = 100
+    default_attenuation = 0.2  # dB/km
+    default_noise_figure = 4.5  # dB
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-k",
+        "--k_paths",
+        type=int,
+        default=default_k_paths,
+        help=f"Number of k-shortest-paths to be considered (default=`{default_k_paths}`)",
+    )
+    parser.add_argument(
+        "-t",
+        "--topology",
+        default=default_topology_file,
+        help=f"Network topology file to be used (default: `{default_topology_file}`)",
+    )
+    parser.add_argument(
+        "-m",
+        "--max_span_length",
+        type=float,
+        default=default_max_span_length,
+        help=f"Maximum span length [km] (default: {default_max_span_length})",
+    )
+    parser.add_argument(
+        "-a",
+        "--attenuation",
+        type=float,
+        default=default_attenuation,
+        help=f"Fiber attenuation (default: {default_attenuation})",
+    )
+    parser.add_argument(
+        "-n",
+        "--noise_figure",
+        type=float,
+        default=default_noise_figure,
+        help=f"Amplifier noise figure (default: {default_attenuation})",
+    )
+
+    args = parser.parse_args()
+
+    topology_path = pathlib.Path(args.topology)
+
+    topology = get_topology(
+        args.topology,
+        topology_path.stem.upper(),
+        cur_modulations,
+        args.max_span_length,
+        args.attenuation,
+        args.noise_figure,
+        args.k_paths,
+    )
+
+    file_name = topology_path.stem + "_" + str(args.k_paths) + "-paths"
+    if cur_modulations is not None:
+        file_name += "_" + str(len(cur_modulations)) + "-modulations"
+    file_name += ".h5"
+
+    output_file = topology_path.parent.resolve().joinpath(file_name)
+    with open(output_file, "wb") as f:
+        pickle.dump(topology, f)
+
+    print("done for", topology)
--- optical_networking_gym/topology.pyx
+++ optical_networking_gym/topology.pyx
@@ -1,17 +1,21 @@
+from itertools import islice
+from typing import Optional, Union, Tuple, Sequence
+
 import cython
+import networkx as nx
 import numpy as np
 
 
 @cython.cclass
 class Span:
 
-    length = cython.declare(cython.double, visibility='readonly')
-    attenuation_db_km = cython.declare(cython.double, visibility='readonly')
-    attenuation_normalized = cython.declare(cython.double, visibility='readonly')
-    noise_figure_db = cython.declare(cython.double, visibility='readonly')
-    noise_figure_normalized = cython.declare(cython.double, visibility='readonly')
+    length = cython.declare(cython.double, visibility="readonly")
+    attenuation_db_km = cython.declare(cython.double, visibility="readonly")
+    attenuation_normalized = cython.declare(cython.double, visibility="readonly")
+    noise_figure_db = cython.declare(cython.double, visibility="readonly")
+    noise_figure_normalized = cython.declare(cython.double, visibility="readonly")
 
-    def __cinit__(self, length: float, attenuation: float, noise_figure: float):
+    def __init__(self, length: float, attenuation: float, noise_figure: float):
         self.length = length
 
         self.attenuation_db_km = attenuation
@@ -29,20 +33,118 @@ class Span:
         self.noise_figure_normalized = 10 ** (self.noise_figure_db / 10)  # dB ===> norm
     
     def __repr__(self) -> str:
-        return f"Span(length={self.length:.2f}, attenuation_db_km={self.attenuation_db_km}, noise_figure_db={self.noise_figure_db})"
+        return f"Span(length={self.length:0.2f}, attenuation_db_km={self.attenuation_db_km}, noise_figure_db={self.noise_figure_db})"
 
 
 @cython.cclass
 class Link:
-    id: cython.declare(cython.int, visibility='readonly')
-    node1: cython.declare(cython.str, visibility='readonly')
-    node2: cython.declare(cython.str, visibility='readonly')
-    length: cython.declare(cython.double, visibility='readonly')
-    spans: tuple[Span] = cython.declare(cython.tuple, visibility='readonly')
+    id = cython.declare(cython.int, visibility="readonly")
+    node1 = cython.declare(cython.str, visibility="readonly")
+    node2 = cython.declare(cython.str, visibility="readonly")
+    length = cython.declare(cython.double, visibility="readonly")
+    spans = cython.declare(cython.tuple[Span], visibility="readonly")
 
-    def __cinit__(self, id: cython.int, node1: cython.str, node2: cython.str, length: cython.double, spans: cython.tuple):
+    def __init__(self, id: cython.int, node1: cython.str, node2: cython.str, length: cython.double, spans: cython.tuple):
         self.id = id
         self.node1 = node1
         self.node2 = node2
         self.length = length
         self.spans = spans
+    
+    def __repr__(self) -> str:
+        return f"Link(id={self.id}, node1={self.node1}, node2={self.node2}, length={self.length:0.2f}, spans={self.spans})"
+
+@cython.cclass
+class Modulation:
+    name = cython.declare(cython.str, visibility="readonly")
+    # maximum length in km
+    maximum_length = cython.declare(cython.float, visibility="readonly")
+    # number of bits per Hz per sec.
+    spectral_efficiency = cython.declare(cython.int, visibility="readonly")
+    # minimum OSNR that allows it to work
+    minimum_osnr = cython.declare(cython.float, visibility="readonly")
+    # maximum in-band cross-talk
+    inband_xt = cython.declare(cython.float, visibility="readonly")
+
+    def __init__(self, name: cython.str, maximum_length: cython.float, spectral_efficiency: cython.int, minimum_osnr: cython.float = 0.0, inband_xt: cython.float = 0.0) -> None:
+        self.name = name
+        self.maximum_length = maximum_length
+        self.spectral_efficiency = spectral_efficiency
+        self.minimum_osnr = minimum_osnr
+        self.inband_xt = inband_xt
+    
+    def __repr__(self) -> str:
+        return f"Modulation(name={self.name}, maximum_length={self.maximum_length}, spectral_efficiency={self.spectral_efficiency}, minimum_osnr={self.minimum_osnr:0.2f}, inband_xt={self.inband_xt:0.2f})"
+
+
+@cython.cclass
+class Path:
+    path_id = cython.declare(cython.int, visibility="readonly")
+    nodes = cython.declare(cython.tuple[cython.str], visibility="readonly")
+    hops = cython.declare(cython.int, visibility="readonly")
+    length = cython.declare(cython.float, visibility="readonly")
+    best_modulation_by_distance = cython.declare(Modulation, visibility="readonly")
+    
+    def __init__(self, path_id: cython.int, nodes: cython.tuple[cython.str], length: cython.float, best_modulation_by_distance: Modulation) -> None:
+        self.path_id = path_id
+        self.nodes = nodes
+        self.hops = len(nodes) - 1
+        self.length = length
+        self.best_modulation_by_distance = best_modulation_by_distance
+    
+    def __repr__(self) -> str:
+        return f"Path(path_id={self.path_id}, nodes={self.nodes}, hops={self.hops}, length={self.length}, best_modulation_by_distance={self.best_modulation_by_distance})"
+
+
+@cython.cclass
+class Service:
+    service_id = cython.declare(cython.int, visibility="readonly")
+    source = cython.declare(cython.str, visibility="readonly")
+    source_id = cython.declare(cython.int, visibility="readonly")
+    destination = cython.declare(cython.str, visibility="readonly")
+    destination_id = cython.declare(cython.int, visibility="readonly")
+    arrival_time = cython.declare(cython.float, visibility="readonly")
+    holding_time = cython.declare(cython.float, visibility="readonly")
+    bit_rate = cython.declare(cython.float, visibility="readonly")
+    path = cython.declare(Path, visibility="public")
+    best_modulation = cython.declare(Modulation, visibility="public")
+    service_class = cython.declare(cython.int, visibility="readonly")
+    number_slots = cython.declare(cython.int, visibility="public")
+    core = cython.declare(cython.int, visibility="public")
+    launch_power = cython.declare(cython.float, visibility="public")
+    accepted = cython.declare(cython.bint, visibility="public")
+
+    # TODO: write the __init__ method
+
+    def __str__(self):
+        # TODO: improve the __str__ method
+        msg = "{"
+        msg += "" if self.bit_rate is None else f"br: {self.bit_rate}, "
+        msg += "" if self.service_class is None else f"cl: {self.service_class}, "
+        return f"Serv. {self.service_id} ({self.source} -> {self.destination})" + msg
+
+
+def get_k_shortest_paths(G: nx.Graph, source: str, target: str, k: int, weight=None):
+    """
+    Method from https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.simple_paths.shortest_simple_paths.html#networkx.algorithms.simple_paths.shortest_simple_paths
+    """
+    return tuple(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
+
+
+def get_path_weight(graph, path, weight="length"):
+    return np.sum([graph[path[i]][path[i + 1]][weight] for i in range(len(path) - 1)])
+
+
+def get_best_modulation_format_by_length(
+    length: float, modulations: Sequence[Modulation]
+) -> Modulation:
+    # sorts modulation from the most to the least spectrally efficient
+    sorted_modulations = sorted(
+        modulations, key=lambda x: x.spectral_efficiency, reverse=True
+    )
+    for i in range(len(modulations)):
+        if length <= sorted_modulations[i].maximum_length:
+            return sorted_modulations[i]
+    raise ValueError(
+        "It was not possible to find a suitable MF for a path with {} km".format(length)
+    )
